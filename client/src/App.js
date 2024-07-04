42584991df133668bf538f23d88a1bdbbf22597d
Daniel Long <danielwlong618@gmail.com>
2024-07-03 19:03:01 -0500

    lab done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Check the console for movies!</h1>;
+}
+
+export default App;
